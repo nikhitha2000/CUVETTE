fda02da6ca21f8facbc7e23d97f77d5b16d04bdd
Nikhitha <nikhibeeraka2000@gmail.com>
2024-06-14 23:53:34 +0530

    pushing code of microproject3

--- microproject3/project3.js
+++ microproject3/project3.js
@@ -1,24 +1,37 @@
+const inputElement = document.getElementById("output");
+const errorElement = document.getElementById("error");
 
-    let currentInput = '';
-    let previousInput = '';
-    let operation = null;
-    
-      let calculation;
-      
-      switch (operation) {
-        case '+':
-          calculation = prev + current;
-          break;
-        case '-':
-          calculation = prev - current;
-          break;
-        case '*':
-          calculation = prev * current;
-          break;
-        case '/':
-          calculation = prev / current;
-          break;
-        default:
-          return;
-      }
-     
+function onButtonClick(event) {
+  const value = event.target.innerText.trim();
+  inputElement.value = inputElement.value + "" + value;
+  hideError();
+}
+
+function onDelete() {
+  if (inputElement.value) {
+    inputElement.value = inputElement.value.slice(0, -1);
+  }
+  hideError();
+}
+
+function onReset() {
+  inputElement.value = "";
+  hideError();
+}
+
+function onEqual() {
+  if (inputElement.value) {
+    try {
+      const input = inputElement.value.replace('X', '*');
+      inputElement.value = Math.round(eval(input), 3);
+    } catch (error) {
+      errorElement.style.display = "block";
+    }
+  }
+}
+
+function hideError() {
+  if (errorElement.style.display !== "none") {
+    errorElement.style.display = "none";
+  }
+}
