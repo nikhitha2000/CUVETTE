cd0a0f15e75551aae550c522163412bdb8d8039f
Nikhitha <nikhibeeraka2000@gmail.com>
2024-06-25 19:29:29 +0530

    working of computer choices

--- mp-1/script.js
+++ mp-1/script.js
@@ -15,3 +15,12 @@ function showRules() {
         console.error('Element with class "rules" not found.');
     }
 }
+function itemclick(){
+getComputerChoice();
+}
+function getComputerChoice(){
+    const choices = ['rock','paper','scissors'];
+    const randomChoice = Math.floor(Math.random() * choices.length);
+    console.log(choices[randomChoice]);
+    return choices[randomChoice];
+}
