74f35dada25bb8b0158efcdf20a90e2d5af0535b
Nikhitha <nikhibeeraka2000@gmail.com>
2024-08-29 17:55:19 +0530

    pedning files

---
+++ REACT/mayx/src/components/W3/Lists.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+
+// const numbers = ["Nikhitha","Beeraka"];
+// const updated = numbers.map((number,index)=>{
+//     return <li key={index}>{number}</li>;
+// })
+function Lists() {
+  return (
+    <div>
+        <ul>
+            <li>Nikhitha</li>
+            <li>Beeraka</li>
+        </ul>
+    </div>
+  )
+}
+
+export default Lists;
