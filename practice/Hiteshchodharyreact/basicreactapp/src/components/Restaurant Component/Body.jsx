239db4b545f21e7f789bb837e1eb15ba99ffac70
Nikhitha <nikhibeeraka2000@gmail.com>
2024-10-17 11:48:12 +0530

    learning

--- practice/Hiteshchodharyreact/basicreactapp/src/components/Restaurant Component/Body.jsx
+++ practice/Hiteshchodharyreact/basicreactapp/src/components/Restaurant Component/Body.jsx
@@ -1,16 +1,28 @@
-import React from 'react';
+// eslint-disable-next-line no-unused-vars
+import React, { useState } from 'react';
 import styles from "../Restaurant Component/Body.module.css";
 import Res from "../Restaurant Component/Res.jsx";
 import restaurantList  from '../../utils/RestaurantList.js';
 function Body() {
+  const[FilteredList,setFilter] = useState(restaurantList);
+  function filterTopRated() {
+    const topRated = restaurantList.filter((restaurant) => {
+      return restaurant.rating >= 4.2;
+    });
+    setFilter(topRated); // Update state with filtered list
+    console.log(topRated);
+  }
+
   return (
+    
     <div className= "body">
-        <div className={styles.search}>
-          Search
+        <div className={styles.filter}>
+          <button className={styles.filterbtn} onClick={filterTopRated}>Top Rated restaurants</button>
+          {/* <button className={styles.delivery} onClick={}>Near & Fast</button> */}
         </div>
         <div className={styles.restrocontainer}>
           {
-            restaurantList.map((restaurant,index)=>(
+            FilteredList.map((restaurant,index)=>(
               <Res
               key = {index}
               resName = {restaurant.resName}
